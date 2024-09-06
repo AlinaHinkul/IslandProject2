396aae900347b04b86896cacb3c19068eeabea3a
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:19:52 +0100

    Update Main.java

--- src/java/com.javarush.island/service/Main.java
+++ src/java/com.javarush.island/service/Main.java
@@ -1,2 +1,10 @@
-package com.javarush.island.service;public class Main {
+package com.javarush.island.service;
+
+import com.javarush.island.activity.Management;
+
+public class Main {
+    public static void main(String[] args) {
+        Management management = new Management();
+        management.start();
+    }
 }
