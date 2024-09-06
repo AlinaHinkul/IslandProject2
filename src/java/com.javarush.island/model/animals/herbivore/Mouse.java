f33087036a289f9b2c087f04170609aeea846a9c
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:18:34 +0100

    Update Mouse.java

--- src/java/com.javarush.island/model/animals/herbivore/Mouse.java
+++ src/java/com.javarush.island/model/animals/herbivore/Mouse.java
@@ -1,2 +1,19 @@
-package com.javarush.island.model.animals.herbivore;public class Mouse {
+package com.javarush.island.model.animals.herbivore;
+
+import com.javarush.island.model.animals.abstracts.HerbivoreAnimal;
+
+public class Mouse extends HerbivoreAnimal {
+    public Mouse() {
+        super(0.05, 500, 1, 0.01);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
