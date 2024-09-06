dbc2ea0444f30ccebd6bd188856358f94d325e7a
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:16:41 +0100

    Update Deer.java

--- src/java/com.javarush.island/model/animals/herbivore/Deer.java
+++ src/java/com.javarush.island/model/animals/herbivore/Deer.java
@@ -1,2 +1,20 @@
-package com.javarush.island.model.animals.herbivore;public class Deer {
+package com.javarush.island.model.animals.herbivore;
+
+import com.javarush.island.model.animals.abstracts.HerbivoreAnimal;
+
+public class Deer extends HerbivoreAnimal {
+    public Deer() {
+        super(300, 20, 4, 50);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
+
