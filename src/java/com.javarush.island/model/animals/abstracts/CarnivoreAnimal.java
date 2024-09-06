07062fb920a719e67b43af2f74a7ab96f4d7cc02
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:09:57 +0100

    Update CarnivoreAnimal.java

--- src/java/com.javarush.island/model/animals/abstracts/CarnivoreAnimal.java
+++ src/java/com.javarush.island/model/animals/abstracts/CarnivoreAnimal.java
@@ -1,2 +1,18 @@
-package com.javarush.island.model.animals.abstracts;public class CarnivoreAnimal {
+package com.javarush.island.model.animals.abstracts;
+
+public abstract class CarnivoreAnimal extends Animal {
+    protected CarnivoreAnimal(double weight, int maxCountOnLocation, int travelSpeed, double maxCountSaturation) {
+        super(weight, maxCountOnLocation, travelSpeed, maxCountSaturation);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
+
