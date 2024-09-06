8e86e4b7d89df351d0066f1661178beb27f5fb59
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:10:17 +0100

    Update HerbivoreAnimal.java

--- src/java/com.javarush.island/model/animals/abstracts/HerbivoreAnimal.java
+++ src/java/com.javarush.island/model/animals/abstracts/HerbivoreAnimal.java
@@ -1,2 +1,17 @@
-package com.javarush.island.model.animals.abstracts;public class HerbivoreAnimal {
+package com.javarush.island.model.animals.abstracts;
+
+public abstract class HerbivoreAnimal extends Animal {
+    protected HerbivoreAnimal(double weight, int maxCountOnLocation, int travelSpeed, double maxCountSaturation) {
+        super(weight, maxCountOnLocation, travelSpeed, maxCountSaturation);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
