c8ccb408116e6eda2226b094dabac72b07706043
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:19:31 +0100

    Update Plant.java

--- src/java/com.javarush.island/model/plant/Plant.java
+++ src/java/com.javarush.island/model/plant/Plant.java
@@ -1,2 +1,15 @@
-package com.javarush.island.model.plant;public class Plant {
+package com.javarush.island.model.plant;
+
+import com.javarush.island.model.animals.abstracts.ObjectIsland;
+
+public class Plant extends ObjectIsland {
+    public Plant() {
+        super(1, 200);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
 }
+
