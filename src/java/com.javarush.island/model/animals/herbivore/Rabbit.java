cc917dc4ea3c09dea25f9d238f55ed0a75f8c2dc
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:18:51 +0100

    Update Rabbit.java

--- src/java/com.javarush.island/model/animals/herbivore/Rabbit.java
+++ src/java/com.javarush.island/model/animals/herbivore/Rabbit.java
@@ -1,2 +1,20 @@
-package com.javarush.island.model.animals.herbivore;public class Rabbit {
+package com.javarush.island.model.animals.herbivore;
+
+import com.javarush.island.model.animals.abstracts.HerbivoreAnimal;
+
+public class Rabbit extends HerbivoreAnimal {
+    public Rabbit() {
+        super(2, 150, 2, 0.45);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
+
