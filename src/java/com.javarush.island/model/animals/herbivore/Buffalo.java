4f20d06c193dba00c385fd45a7c31554121b3ffb
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:15:43 +0100

    Update Buffalo.java

--- src/java/com.javarush.island/model/animals/herbivore/Buffalo.java
+++ src/java/com.javarush.island/model/animals/herbivore/Buffalo.java
@@ -1,2 +1,19 @@
-package com.javarush.island.model.animals.herbivore;public class Buffalo {
+package com.javarush.island.model.animals.herbivore;
+
+import com.javarush.island.model.animals.abstracts.HerbivoreAnimal;
+
+public class Buffalo extends HerbivoreAnimal {
+    public Buffalo() {
+        super(700, 10, 3, 100);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
