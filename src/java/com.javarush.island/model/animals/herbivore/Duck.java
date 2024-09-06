a2c3cee352224e5143ec5bdc91fc21c8af61db61
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:17:03 +0100

    Update Duck.java

--- src/java/com.javarush.island/model/animals/herbivore/Duck.java
+++ src/java/com.javarush.island/model/animals/herbivore/Duck.java
@@ -1,2 +1,19 @@
-package com.javarush.island.model.animals.herbivore;public class Duck {
+package com.javarush.island.model.animals.herbivore;
+
+import com.javarush.island.model.animals.abstracts.HerbivoreAnimal;
+
+public class Duck extends HerbivoreAnimal {
+    public Duck() {
+        super(1, 200, 4, 0.15);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
