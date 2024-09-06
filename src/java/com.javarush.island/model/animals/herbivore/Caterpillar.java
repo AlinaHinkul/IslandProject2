68eba412b5e86ff08922edf7c0d33a149e6de3d5
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:16:18 +0100

    Update Caterpillar.java

--- src/java/com.javarush.island/model/animals/herbivore/Caterpillar.java
+++ src/java/com.javarush.island/model/animals/herbivore/Caterpillar.java
@@ -1,2 +1,20 @@
-package com.javarush.island.model.animals.herbivore;public class Caterpillar {
+package com.javarush.island.model.animals.herbivore;
+
+import com.javarush.island.model.animals.abstracts.HerbivoreAnimal;
+
+public class Caterpillar extends HerbivoreAnimal {
+    public Caterpillar() {
+        super(0.01, 1000, 0, 0);
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
+
