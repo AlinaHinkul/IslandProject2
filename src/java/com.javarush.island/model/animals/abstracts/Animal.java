9efb3c336d34a5f2633ba4a585fce49b440feb72
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:09:35 +0100

    Update Animal.java

--- src/java/com.javarush.island/model/animals/abstracts/Animal.java
+++ src/java/com.javarush.island/model/animals/abstracts/Animal.java
@@ -1,2 +1,27 @@
-package com.javarush.island.model.animals.abstracts;public class Animal {
+package com.javarush.island.model.animals.abstracts;
+
+public abstract class Animal extends ObjectIsland {
+    private int travelSpeed;
+    private double maxCountSaturation;
+
+    protected Animal(double weight, int maxCountOnLocation, int travelSpeed, double maxCountSaturation) {
+        super(weight, maxCountOnLocation);
+        this.travelSpeed = travelSpeed;
+        this.maxCountSaturation = maxCountSaturation;
+    }
+
+    public int getTravelSpeed() {
+        return travelSpeed;
+    }
+
+    public double getMaxCountSaturation() {
+        return maxCountSaturation;
+    }
+
+    @Override
+    public abstract void reproduce();
+
+    public abstract void eat();
 }
+
+
