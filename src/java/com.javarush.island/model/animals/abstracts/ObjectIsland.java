87966e63f54bf2a1339bd2433b912f7a3bc54583
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-06 20:10:36 +0100

    Update ObjectIsland.java

--- src/java/com.javarush.island/model/animals/abstracts/ObjectIsland.java
+++ src/java/com.javarush.island/model/animals/abstracts/ObjectIsland.java
@@ -1,2 +1,22 @@
-package com.javarush.island.model.animals.abstracts;public class ObjectIsland {
+package com.javarush.island.model.animals.abstracts;
+
+public abstract class ObjectIsland {
+    private double weight;
+    private int maxCountOnLocation;
+
+    protected ObjectIsland(double weight, int maxCountOnLocation) {
+        this.weight = weight;
+        this.maxCountOnLocation = maxCountOnLocation;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getMaxCountOnLocation() {
+        return maxCountOnLocation;
+    }
+
+    public abstract void reproduce();
 }
+
